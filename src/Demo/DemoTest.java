81e88a8849d77f99b51d44970508fbe2a1804eeb
unknown <nghia.huynh@lgvn13199.logigear.com>
2016-01-19 14:29:37 +0700

    Update TestNG lib

--- src/Demo/DemoTest.java
+++ src/Demo/DemoTest.java
@@ -1,21 +1,13 @@
 package Demo;
 
-import org.testng.annotations.Test;
-
-import core.SeleniumDriver;
-
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
+import org.testng.annotations.*;
+
+import core.SeleniumDriver;
+
 
 public class DemoTest {
 
